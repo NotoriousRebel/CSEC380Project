73e96fc4ae089c1e985b7fe9d94166129c5f2050
Jack <33186556+JackHyland@users.noreply.github.com>
2019-10-29 20:28:16 +0000

    update test

--- tests/test_act3.py
+++ tests/test_act3.py
@@ -5,7 +5,7 @@ import requests
 def test_helloworld():
     # Basic pytest for act1 to determine if travis is working
     resp = requests.get('http://localhost')
-    assert 'Hello World' in resp.text
+    assert 'SIGN-IN HERE' in resp.text
     assert 200 == resp.status_code
 
 
