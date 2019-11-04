4241d5713585dff0155cc64cff026b937ba8ce96
NotoriousRebel <matt12299@yahoo.com>
2019-11-04 14:12:39 -0500

    Fixed travis build failing due to using port 82 for web server.

--- tests/test_act3.py
+++ tests/test_act3.py
@@ -4,7 +4,8 @@ import requests
 
 def test_helloworld():
     # Basic pytest for act1 to determine if travis is working
-    resp = requests.get('http://localhost')
+    resp = requests.get('http://localhost:82')
+    # Port 82 due to port mapping
     assert 'SIGN-IN HERE' in resp.text
     assert 200 == resp.status_code
 
