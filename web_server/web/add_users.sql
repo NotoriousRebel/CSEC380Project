660dfeebdb315d6944255c800692cba684a1fc93
JackHyland <33186556+JackHyland@users.noreply.github.com>
2019-11-14 12:40:32 -0800

    fixed missing files for webserevre

---
+++ web_server/web/add_users.sql
@@ -0,0 +1,5 @@
+-- Adding sample users 
+INSERT INTO Credentials (ID, UserName, Password) VALUES (NULL, "jack", "pass");
+INSERT INTO Credentials (ID, UserName, Password) VALUES (NULL, "pete", "S3cuRe");
+INSERT INTO Credentials (ID, UserName, Password) VALUES (NULL, "sam", "mark");
+INSERT INTO Credentials (ID, UserName, Password) VALUES (NULL, "zuckstuff", "markymark");
