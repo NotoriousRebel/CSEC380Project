68dcbc90632c031578c81c436413573ef7ce43ee
Duncedrin <evdelmolino@gmail.com>
2019-11-23 09:38:15 -0500

    fixing pytest warnings

--- tests/test_act6.py
+++ tests/test_act6.py
@@ -7,8 +7,6 @@ try:
 except Exception as e:
     print(f'An exception has occurred while disabling warnings: {e}')
 
-
-
 def test_ssrf():
     #set user
     session = requests.session()
@@ -33,30 +31,30 @@ def test_ssrf():
     login_url = 'https://127.0.0.1/login.php'
     session.post(login_url, data=dataSetB, verify=False)  
     #the actual tests
-    #test1
     dataSet1 = {
         'upload-submitlink': "Send data",
         'videolink': "http://evnet.student.rit.edu:80/test.mp4"
     }
-    r1 = session.post("https://127.0.0.1/upload.php",data=dataSet1, verify=False)
-    #test2
+    url1 = "https://127.0.0.1/upload.php"
+    r1 = session.post(url1, data=dataSet1, verify=False)
     dataSet2 = {
         'upload-submitlink': "Send data",
         'videolink': "http://evnet.student.rit.edu:22/test.mp4"
     }
-    r2 = session.post("https://127.0.0.1/upload.php",data=dataSet2, verify=False)
-    #test3
+    url2 = "https://127.0.0.1/upload.php"
+    r2 = session.post(url2, data=dataSet2, verify=False)
     dataSet3 = {
         'upload-submitlink': "Send data",
         'videolink': "http://evnet.student.rit.edu:400/test.mp4"
     }
-    r3 = session.post("https://127.0.0.1/upload.php",data=dataSet3, verify=False)
-    #test4
+    url3 = "https://127.0.0.1/upload.php"
+    r3 = session.post(url3, data=dataSet3, verify=False)
     dataSet4 = {
         'upload-submitlink': "Send data",
         'videolink': "http://evnet.student.rit.edu:443/test.mp4"
     }
-    r4 = session.post("https://127.0.0.1/upload.php",data=dataSet4, verify=False)
+    url4 = "https://127.0.0.1/upload.php"
+    r4 = session.post(url4, data=dataSet4, verify=False)
     #verify tests
     assert "Welcome to Memetube" in r1.text
     assert "Error moving file" in r2.text
